a55702e1ae7df3b0e49f66484fca30839c71e172
masanori-komiya <little_temple@icloud.com>
2021-01-13 22:38:16 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 小宮聖智 on 2021/01/13.
+//  Copyright © 2021 masanori.komiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
