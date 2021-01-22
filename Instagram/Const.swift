a2da91051191d8032f38c44312ecd6d04368686c
masanori-komiya <little_temple@icloud.com>
2021-01-22 15:31:05 +0900

    instagram

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 小宮聖智 on 2021/01/22.
+//  Copyright © 2021 masanori.komiya. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
